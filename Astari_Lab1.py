1d7dc2c1086e94de3f4b957b65427e3d7f22256f
Astari Raihanah <astari@uchicago.edu>
2024-03-21 09:56:09 -0500

    Create Astari_Lab1.py

---
+++ Astari_Lab1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 06:46:52 2024
+
+@author: Astari Raihanah
+"""
+
+print('Hello world!')
+print('This is the first code of Astari in the DaPP I')
+x = 10
+
+print(x*2)
